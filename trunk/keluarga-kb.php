64463e00a25f0bc99ec47977b2d87a4f140ef28b
kierjarat@gmail.com <kierjarat@gmail.com@5d860d71-5b96-3e2d-c380-4e86694a2747>
2013-01-15 09:15:05 +0000

    added new files (all files still a dummy): - jenis-kontrasepsi.php - keluarga-kb.php - keluarga.php - tempat-pemeriksaan.php
    added new js file:
    - custom.js
    	=> loadContent, with 1 parameter
    		url: path of file content
    	=> addTab, with 3 parameters
    		id: name of the placeholder element
    		node: name of the tree title (onclick)
    		url: path of file content

---
+++ trunk/keluarga-kb.php
@@ -0,0 +1,3 @@
+<?php
+	echo 'keluarga-kb';
+?>
--- trunk/main.php
+++ trunk/main.php
@@ -1,4 +1,8 @@
 <!-- author: kierjarat -->
+<!-- 
+	data-options => href, url
+-->
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,6 +13,7 @@
 		<link rel="stylesheet" type="text/css" href="js/jquery-easyui-1.3.2/demo/demo.css">
 		<script type="text/javascript" src="js/jquery-easyui-1.3.2/jquery-1.8.0.min.js"></script>
 		<script type="text/javascript" src="js/jquery-easyui-1.3.2/jquery.easyui.min.js"></script>
+		<script type="text/javascript" src="js/custom.js"></script>
 	</head>
 	<body>	
 		<div style="margin:10px 0;"></div>
@@ -17,19 +22,57 @@
 			<div data-options="region:'south',split:true" style="height:50px;">FOOTER</div>
 			<div data-options="region:'west',split:true" title="Panel Menu" style="width:200px;">
 				<div class="easyui-accordion" data-options="fit:true,border:false">
+<<<<<<< .mine
+					<div title="MDK" style="padding:10px;">					
+						<ul id="menu-tree" class="easyui-tree" data-options="url:'json/menu-tree.json',animate:true,dnd:true"></ul>		
+					</div>					
+					<div title="Sistem" style="padding:10px">
+						<ul class="easyui-tree" data-options="url:'json/menu-sistem-tree.json',animate:true,dnd:true"></ul>
+=======
 					<div title="MDK" style="padding:10px;">					
 						<ul class="easyui-tree" data-options="url:'json/menu-tree.json',animate:true,dnd:true"></ul>		
 					</div>					
 					<div title="Sistem" style="padding:10px">
 						<ul class="easyui-tree" data-options="url:'json/menu-sistem-tree.json',animate:true,dnd:true"></ul>
+>>>>>>> .r7
 					</div>
 				</div>
 			</div>
+<<<<<<< .mine
+			<div id="content" class="easyui-tabs" data-options="region:'center',title:'Dashboard',iconCls:'icon-ok'"></div>
+=======
 			<div data-options="region:'center',title:'Dashboard',iconCls:'icon-ok'">
 				<div class="easyui-tabs" data-options="fit:true,border:false,plain:true">
 					<div title="About" data-options="href:'js/jquery-easyui-1.3.2/demo/layout/_content.html'" style="padding:10px"></div>				
 				</div>
 			</div>
+>>>>>>> .r7
 		</div>
+
+		<script>
+			$(function() {
+				$("#menu-tree").tree({
+					onClick: function(node) {
+						switch (node.text) {
+							case "Keluarga" : {
+								addTab($("#content"), node.text, "keluarga.php");
+							} break;
+							
+							case "Keluarga KB" : {
+								addTab($("#content"), node.text, "keluarga-kb.php");
+							} break;
+
+							case "Jenis Kontrasepsi" : {
+								addTab($("#content"), node.text, "jenis-kontrasepsi.php");
+							} break;
+
+							case "Tempat Pemeriksaan" : {
+								addTab($("#content"), node.text, "tempat-pemeriksaan.php");
+							} break;
+						}
+					}
+				})
+			});			
+		</script>
 	</body>
 </html>
