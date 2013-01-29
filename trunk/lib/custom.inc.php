51eada618a987722789f63c2cdfd196dc0196849
kierjarat@gmail.com <kierjarat@gmail.com@5d860d71-5b96-3e2d-c380-4e86694a2747>
2013-01-29 01:32:07 +0000

    remove all source codes

--- trunk/lib/custom.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-  
-  function checkSession() {
-	  if ( !isset( $_SESSION[ 'username' ] ) || $_SESSION[ 'username' ] == '' ) {
-	    echo '<script>window.location.replace("http://localhost/mdk-php/");</script>';
-	  }
-  }
-
-?>
